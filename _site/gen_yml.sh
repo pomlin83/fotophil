84c46aec84e5028d00bd19f477846890323aa66e
Philip Omlin <fil@fil.home>
2020-04-13 19:56:32 +0200

    Changes Navigation Header

--- _site/gen_yml.sh
+++ _site/gen_yml.sh
@@ -6,10 +6,19 @@ echo "# Generation Date:" $time > _data/carousel.yml
 echo "# Author: Philip Omlin" >> _data/carousel.yml
 echo "# Image source: " $1 >> _data/carousel.yml
 echo "images:" >> _data/carousel.yml
-
+i=1
 for img in $1/normal_l-*.jpg; do
-echo " - url:" $img >> _data/carousel.yml
-echo "   title: <title>"  >> _data/carousel.yml
-echo "   caption: <caption>" >> _data/carousel.yml
-echo "   subcaption: <subcaption>" >> _data/carousel.yml
+ echo " - normal:" $img >> _data/carousel.yml
+ if [ $i -lt 10 ]; then
+  echo "   sm: $1/sm_p-0$i.jpg" >> _data/carousel.yml
+  echo "   tp: $1/tp_p-0$i.jpg" >> _data/carousel.yml
+  echo "   wl: $1/wide_l-0$i.jpg" >> _data/carousel.yml
+ else
+  echo "   sm: $1/sm_p-$i.jpg" >> _data/carousel.yml
+  echo "   tp: $1/tp_p-$i.jpg" >> _data/carousel.yml
+  echo "   wl: $1/wide_l-$i.jpg" >> _data/carousel.yml
+ fi
+ echo "   title: Architecture"  >> _data/carousel.yml
+ echo "   caption: Architecture" >> _data/carousel.yml
+ ((i=i+1))
 done
