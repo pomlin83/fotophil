008a37273101be41c8fb9440e0190521031d4aa0
Philip Omlin <fil@fil.home>
2020-04-14 22:27:19 +0200

    changes in galery

--- _site/gen_yml_galery.sh
+++ _site/gen_yml_galery.sh
@@ -9,15 +9,6 @@ echo "images:" >> _data/galery.yml
 i=1
 for img in $1main_normal_l-*.jpg; do
  echo " - normal:" $img >> _data/galery.yml
- if [ $i -lt 10 ]; then
-  echo "   sm: $1sm_p-$i.jpg" >> _data/galery.yml
-  echo "   tp: $1tp_p-$i.jpg" >> _data/galery.yml
-  echo "   wl: $1wide_l-$i.jpg" >> _data/galery.yml
- else
-  echo "   sm: $1sm_p-$i.jpg" >> _data/galery.yml
-  echo "   tp: $1tp_p-$i.jpg" >> _data/galery.yml
-  echo "   wl: $1wide_l-$i.jpg" >> _data/galery.yml
- fi
  echo "   title: Architecture"  >> _data/galery.yml
  echo "   caption: Architecture" >> _data/galery.yml
  ((i=i+1))
